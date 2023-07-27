9ad9e277c9f6f31672565fbded400006bb60d6aa
Iurii Kurinnoi <yur4kur@icloud.com>
2023-07-27 22:58:54 +0300

    Создана структура папок, добавлены файлы под модели и источник данных.

---
+++ UiLuxury_TeamProject/Models/Items.swift
@@ -0,0 +1,8 @@
+//
+//  Items.swift
+//  UiLuxury_TeamProject
+//
+//  Created by Юрий Куринной on 27.07.2023.
+//
+
+import Foundation
---
+++ UiLuxury_TeamProject/Services/DataSource.swift
@@ -0,0 +1,8 @@
+//
+//  DataSource.swift
+//  UiLuxury_TeamProject
+//
+//  Created by Юрий Куринной on 27.07.2023.
+//
+
+import Foundation
