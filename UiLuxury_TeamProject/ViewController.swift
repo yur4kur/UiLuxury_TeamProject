8d006eeff8236b42cf1414fcc172043dcde6af42
Iurii Kurinnoi <yur4kur@icloud.com>
2023-07-27 22:51:17 +0300

    Initial Commit

---
+++ UiLuxury_TeamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiLuxury_TeamProject
+//
+//  Created by Юрий Куринной on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
