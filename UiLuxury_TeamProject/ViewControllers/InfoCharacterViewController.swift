d1b2237812752d23e0693261268f614462d84b69
biibolzulpukarov <biybol07@gmail.com>
2023-07-28 14:50:55 +0600

    Storyboard First Step

---
+++ UiLuxury_TeamProject/ViewControllers/InfoCharacterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoCharacterViewController.swift
+//  UiLuxury_TeamProject
+//
+//  Created by Бийбол Зулпукаров on 28/7/23.
+//
+
+import UIKit
+
+class InfoCharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
