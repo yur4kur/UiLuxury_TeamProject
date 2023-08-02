d62cb73dcafbf629c2d69999a51888396a474573
Iurii Kurinnoi <yur4kur@icloud.com>
2023-08-02 19:52:33 +0300

    Изменен файл модели: поправлено название файла, поправлены наименования свойств, добавлены новые наименования. В ClickVC прописан IBAction.

--- UiLuxury_TeamProject/Services/DataSource.swift
+++ UiLuxury_TeamProject/Services/DataSource.swift
@@ -5,4 +5,96 @@
 //  Created by Юрий Куринной on 27.07.2023.
 //
 
-import Foundation
+class DataSource {
+    static let shared = DataSource()
+    
+    let gameItems = [
+        Item(
+            title: "Homeless hat",
+            price: 15,
+            description: "Plus one point to any tap",
+            modifier: 1,
+            actionOperator: .add),
+        
+        Item(
+            title: "Lost talent",
+            price: 40,
+            description: "Multiplies the number of points per touch by 2",
+            modifier: 2,
+            actionOperator: .multiply),
+       
+        Item(
+            title: "Back to ex-girlfriend",
+            price: 80,
+            description: "Plus random point per one tap. There is a chance to lose 50 points at once",
+            modifier: Int.random(in: 1...50),
+            actionOperator: .add),
+
+        Item(
+            title: "New haircut",
+            price: 80,
+            description: "Plus three points to any tap",
+            modifier: 3,
+            actionOperator: .add),
+
+        Item(title: "Red-Bull",
+             price: 140,
+             description: "Multiply point from one tap by 3",
+             modifier: 3,
+             actionOperator: .multiply),
+
+        Item(title: "Lotery ticket",
+             price: 140,
+             description: "Delete all your items and take random item from shop",
+             modifier: 1,
+             actionOperator: .assets),
+        
+        Item(title: "Illegal business",
+             price: 140,
+             description: "Take 500 points, but locked one item slot ",
+             modifier: 1,
+             actionOperator: .assets),
+
+        Item(title: "Mortgage",
+             price: 200,
+             description: "Multiply your score wallet and lock one item slot",
+             modifier: 1,
+             actionOperator: .assets),
+
+        Item(title: "Good job",
+             price: 200,
+             description: "Plus 7 point per one tap",
+             modifier: 7,
+             actionOperator: .add),
+
+        Item(title: "Psychologist",
+             price: 250,
+             description: "Multiplies the number of points per touch by 4",
+             modifier: 4,
+             actionOperator: .multiply),
+
+        Item(title: "New Love",
+             price: 250,
+             description: "Random points per one tap. There is a chance lost 100 points per tap",
+             modifier: 100,
+             actionOperator: .add),
+
+        Item(title: "Release a rock album",
+             price: 500,
+             description: "Take a random huge point, or lose all. If you sell, get -200 points",
+             modifier: 200,
+             actionOperator: .add),
+
+        Item(title: "Defeat Mom's Friend's Son",
+             price: 1000,
+             description: "Take a god state and plus 20 points to any tap. ",
+             modifier: 20,
+             actionOperator: .add),
+
+        Item(title: "Win this game and take credits",
+             price: 6000,
+             description: "Good Job",
+             modifier: 1,
+             actionOperator: .assets)
+    ]
+}
--- UiLuxury_TeamProject/ViewControllers/ClickViewController.swift
+++ UiLuxury_TeamProject/ViewControllers/ClickViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 
 class ClickViewController: UIViewController {
 
-    // MARK: - Override methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+    let items: [Item] = DataSource.shared.gameItems
     
-
+    var score = 0
+   
+    
     
     // MARK: - Navigation
 
@@ -25,5 +23,19 @@ class ClickViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     
-
+    @IBAction func clickerTapped() {
+        score += 1
+        items.forEach { item in
+            switch item.itemOperator {
+            case .add:
+                score += item.itemModifier
+            case .multiply:
+                score *= item.itemModifier
+            case .assets:
+                return
+            }
+        }
+        print(score)
+    }
+    
 }
