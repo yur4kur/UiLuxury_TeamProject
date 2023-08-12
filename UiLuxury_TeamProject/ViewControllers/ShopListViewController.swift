56de03451079543b2a36168ec05d1a75bc4d5ea7
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-08-12 17:55:20 +0300

    Написал логику магазина и корзины

--- UiLuxury_TeamProject/ViewControllers/ShopListViewController.swift
+++ UiLuxury_TeamProject/ViewControllers/ShopListViewController.swift
@@ -7,83 +7,75 @@
 
 import UIKit
 
-class ShopListViewController: UITableViewController {
-
+final class ShopListViewController: UITableViewController {
+    
+    //MARK: - Private propertys
+    
+    private let shoppings = Item.getItem()
+    private var selectedCells: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        shoppings.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        1
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellMarket", for: indexPath)
+        let content = shoppings[indexPath.section]
+        cell.textLabel?.text = content.description
+        cell.detailTextLabel?.text = "$\(content.price.formatted())"
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        shoppings[section].title
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = .systemGray6
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedCell = shoppings[indexPath.section]
+        
+        if selectedCells.count < 3 {
+            selectedCells.append(selectedCell)
+        } else {
+            let alert = UIAlertController(
+                title: "Упс",
+                message: "В корзину можно добавить только ТРИ апгрейда",
+                preferredStyle: .alert)
+            let okAktion = UIAlertAction(title: "Ок", style: .default)
+            alert.addAction(okAktion)
+            present(alert, animated: true)
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let basketListVS = segue.destination as? BasketListViewController else { return }
+        basketListVS.selectedCells = selectedCells
+        basketListVS.delegate = self
     }
-    */
+}
 
+//MARK: - Update Data Delegate
+
+extension ShopListViewController: IUpdateDataDelegate {
+    func updateData(updatedArray: [Item]) {
+        self.selectedCells = updatedArray
+    }
 }
