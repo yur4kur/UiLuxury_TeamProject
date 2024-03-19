1051ed3ee0fbd23c9df36a6cfdad8866932ff670
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2024-03-19 14:42:43 +0500

    1 - InfoCharacterViewController, DevelopersInfoViewController: рефакоринг кода, 2 - InfoCharacterViewController: изменен алгоритм определения изображения, добавлены звуки сценариев levelUp и levelDown, работа над отображением изображения и количества кредитов, 3 - Services: добавлен менеджер добавления звуков, 4 - Resourсes: добавлены звуки levelUp и levelDown.

--- UiLuxury_TeamProject/ViewControllers/DevelopersInfoViewController.swift
+++ UiLuxury_TeamProject/ViewControllers/DevelopersInfoViewController.swift
@@ -11,111 +11,117 @@ import UIKit
 
 /// ViewController отображения информации о разработчиках
 final class DevelopersInfoViewController: UIViewController {
-
+    
     // MARK: - Private properties
-
+    
     /// Сегмент-контроллер для переключения между карточками о разработчиках
     private var developerSegments = UISegmentedControl()
-
+    
     /// Изображение разработчика
     private let developerImageView = UIImageView()
-
-    /// Контактная информация разработчика
-    private let developerContactLabel = UILabel()
-
-    /// Индекс выбранного сегмента
+    
+    /// Кнопка перехода в Telegram
+    private let telegramButton = UIButton()
+    
+    /// Текущий индекс сегмента
     private var segmentIndex = 0
-
+    
+    /// Текущая Telegram-ссылка разработчика
+    private var currentURL = "https://t.me/AkiraReiTyan"
+    
     // MARK: - Lifecycle methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupBinding()
     }
-
+    
     // MARK: - Private Methods
-
+    
     /// Метод настройки карточки разработчика
     @objc private func showDeveloperInfo() {
         let selectedSegmentIndex = developerSegments.selectedSegmentIndex
         guard selectedSegmentIndex < DevelopersInfo.names.count else { return }
         segmentIndex = selectedSegmentIndex
-
+        
         guard let developerImage = UIImage(named: String(segmentIndex)) else { return }
         let developerContact = DevelopersInfo.contacts[segmentIndex]
-
+        
         developerImageView.image = developerImage
-        developerContactLabel.text = developerContact
+        currentURL = developerContact
+    }
+    
+    /// Метод перехода в Telegram
+    @objc private func openURL() {
+        guard let url = URL(string: currentURL) else { return }
+        UIApplication.shared.open(url)
     }
 }
 
 // MARK: - Configure UI
 
 private extension DevelopersInfoViewController {
-
+    
     /// Метод настройки пользовательского интерфейса
     func setupUI() {
         setupView()
         setupDeveloperSegments()
         setupDeveloperImageView()
-        setupDeveloperContactLabel()
-
+        setupTelegramButton()
+        
         addSubviews()
         setConstraints()
     }
-
-    /// Метод добавления действия кнопке сегмента
+    
+    /// Метод добавления действий  элементам интерфейса
     func setupBinding() {
-        developerSegments.addTarget(
-            self,
-            action: #selector(showDeveloperInfo),
-            for: .valueChanged
-        )
+        developerSegments.addTarget(self, action: #selector(showDeveloperInfo), for: .valueChanged)
+        telegramButton.addTarget(self, action: #selector(openURL), for: .touchUpInside)
     }
 }
 
 // MARK: - Setup UI
 
 private extension DevelopersInfoViewController {
-
+    
     /// Метод настройки главного экрана
     func setupView() {
         view.addQuadroGradientLayer()
     }
-
+    
     /// Метод настройки сегмент-контроллера
     func setupDeveloperSegments() {
         developerSegments = UISegmentedControl(items: DevelopersInfo.names)
         developerSegments.selectedSegmentIndex = segmentIndex
         showDeveloperInfo()
     }
-
+    
     /// Метод настройки изображения пользователя
     func setupDeveloperImageView() {
         developerImageView.contentMode = .scaleAspectFill
         developerImageView.layer.cornerRadius = 8
         developerImageView.clipsToBounds = true
     }
-
-    /// Метод настройки текста контактной информации
-    func setupDeveloperContactLabel() {
-        developerContactLabel.font = UIFont.systemFont(ofSize: 17)
-        developerContactLabel.textColor = .black
-        developerContactLabel.textAlignment = .left
+    
+    /// Метод настройки изображения кнопки перехода в Telegram
+    func setupTelegramButton() {
+        guard let image = UIImage(named: "logo_telegram") else { return }
+        telegramButton.setImage(image, for: .normal)
     }
-
+    
     /// Метод добавления элементов интерфейса на главный экран и отключения масок AutoLayout
     func addSubviews() {
         view.addSubviews(
             developerSegments,
             developerImageView,
-            developerContactLabel
+            telegramButton
         )
+        
         view.disableAutoresizingMask(
             developerSegments,
             developerImageView,
-            developerContactLabel
+            telegramButton
         )
     }
 }
@@ -123,7 +129,7 @@ private extension DevelopersInfoViewController {
 // MARK: - Constraints
 
 private extension DevelopersInfoViewController {
-
+    
     /// Метод установки констреинтов элементов интерфейса
     func setConstraints() {
         NSLayoutConstraint.activate([
@@ -131,16 +137,16 @@ private extension DevelopersInfoViewController {
             developerSegments.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             developerSegments.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             developerSegments.heightAnchor.constraint(equalToConstant: 32),
-
+            
             developerImageView.topAnchor.constraint(equalTo: developerSegments.bottomAnchor, constant: 20),
             developerImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             developerImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             developerImageView.widthAnchor.constraint(equalTo: developerImageView.heightAnchor, multiplier: 1),
-
-            developerContactLabel.topAnchor.constraint(equalTo: developerImageView.bottomAnchor, constant: 20),
-            developerContactLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            developerContactLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            developerContactLabel.heightAnchor.constraint(equalToConstant: 22)
+            
+            telegramButton.trailingAnchor.constraint(equalTo: developerImageView.trailingAnchor, constant: -16),
+            telegramButton.bottomAnchor.constraint(equalTo: developerImageView.bottomAnchor, constant: -16),
+            telegramButton.heightAnchor.constraint(equalToConstant: 30),
+            telegramButton.widthAnchor.constraint(equalToConstant: 30)
         ])
     }
 }
@@ -148,7 +154,7 @@ private extension DevelopersInfoViewController {
 // MARK: - Constants
 
 private extension DevelopersInfoViewController {
-
+    
     /// Информация о разработчиках
     enum DevelopersInfo {
         static let names = [
@@ -159,7 +165,7 @@ private extension DevelopersInfoViewController {
             "Эльдар",
             "Рустам"
         ]
-
+        
         static let contacts = [
             "https://t.me/AkiraReiTyan",
             "https://t.me/kizi_mcfly",
--- UiLuxury_TeamProject/ViewControllers/InfoCharacterViewController.swift
+++ UiLuxury_TeamProject/ViewControllers/InfoCharacterViewController.swift
@@ -6,86 +6,123 @@
 //
 
 import UIKit
+import AVFoundation
 
 // MARK: - InfoCharacter ViewController
 
 /// ViewController отображения информации о персонаже
 final class InfoCharacterViewController: UIViewController {
-
+    
     // MARK: - Public properties
     // var delegate: ISendInfoAboutCharacterDelegate?
     // var items: [Item] = DataSource.shared.gameItems
     // var userWallet = 0
-
+    
     // MARK: - Private properties
-
+    
     /// Изображение пользователя
     private var userImageView = UIImageView()
-
+    
     /// Отображение количества кредитов
-    private let walletLabel = UILabel()
-
+    private let userCreditsLabel = UILabel()
+    
     /// Таблица отображения купленных предметов
-    private let itemsTableView = UITableView()
-
+    private let userItemsTableView = UITableView()
+    
+    /// Точка доступа к SoundManager
+    private let soundManager = SoundManager.shared
+    
     /// Экземпляр модели User
     private var user = User.shared
-
+    
+    /// Текущий уровень пользователя
+    private var userStage = 0
+    
     // MARK: - Lifecycle methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        updateUserPic()
         updateCreditsValue()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateUserImage()
+    }
+    
     // MARK: - Private methods
-
-    /// Метод обновления изображения пользователя
-    private func updateUserPic() {
+    
+    /// Метод обновления изображения пользователя с анимацией и воспроизведением звука
+    private func updateUserImage() {
         let credits = user.wallet
-
-        switch credits {
-        case 0...1499:
-            guard let userPic = UIImage(named: "stage01") else { return }
-            userImageView.image = userPic
-        case 1500...2999:
-            guard let userPic = UIImage(named: "stage02") else { return }
-            userImageView.image = userPic
-        case 3000...4499:
-            guard let userPic = UIImage(named: "stage03") else { return }
-            userImageView.image = userPic
-        default:
-            guard let userPic = UIImage(named: "stage04") else { return }
-            userImageView.image = userPic
+        let previousUserStage = userStage
+        
+        userStage = calculateUserStage(from: credits)
+        
+        let stageImage = StageImages.images[userStage]
+        guard let userImage = UIImage(named: stageImage) else { return }
+        userImageView.image = userImage
+        
+        UIView.transition(with: userImageView,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: { [weak self] in
+            self?.userImageView.image = userImage
+        },
+                          completion: nil)
+        
+        if userStage > previousUserStage {
+            play(sound: Sounds.levelUp)
+        } else if userStage < previousUserStage {
+            play(sound: Sounds.levelDown)
         }
     }
-
+    
+    /// Метод определения уровня пользователя
+    private func calculateUserStage(from credits: Int) -> Int {
+        switch credits {
+        case 0...1499:
+            return 0
+        case 1500...2999:
+            return 1
+        case 3000...4499:
+            return 2
+        default:
+            return 3
+        }
+    }
+    
     /// Метод отображения обновленной информации о кредитах в таблице
     private func updateCreditsValue() {
         DispatchQueue.main.async {
-            self.itemsTableView.reloadData()
-            self.walletLabel.text = "Credits: \(self.user.wallet)"
+            self.userItemsTableView.reloadData()
+            self.userCreditsLabel.text = "CREDITS: \(self.user.wallet)"
         }
+    }
+    
+    /// Метод настройки и воспроизведения звука
+    private func play(sound: String) {
+        soundManager.setupAudioPlayer(fromSound: sound)
+        soundManager.audioPlayer?.play()
     }
 }
 
 // MARK: - Configure UI
 
 private extension InfoCharacterViewController {
-
+    
     /// Метод настройки пользовательского интерфейса
     func setupUI() {
         setupView()
         setupUserImageView()
         setupWalletLabel()
-        setupBoughtItemsTableView()
-
+        setupUserItemsTableView()
+        
         addSubviews()
         setConstraints()
     }
@@ -94,47 +131,51 @@ private extension InfoCharacterViewController {
 // MARK: - Setup UI
 
 private extension InfoCharacterViewController {
-
+    
     /// Метод настройки главного экрана
     func setupView() {
         view.addQuadroGradientLayer()
     }
-
+    
     /// Метод настройки изображения пользователя
     func setupUserImageView() {
         userImageView.contentMode = .scaleAspectFit
+        userImageView.layer.shadowColor = UIColor.black.cgColor
+        userImageView.layer.shadowRadius = 5
+        userImageView.layer.shadowOpacity = 0.15
+        userImageView.layer.shadowOffset = CGSize(width: 5, height: 5)
     }
-
+    
     /// Метод настройки отображения количества кредитов
     func setupWalletLabel() {
-        walletLabel.font = .systemFont(ofSize: 17)
-        walletLabel.textColor = .black
-        walletLabel.textAlignment = .center
+        userCreditsLabel.font = .boldSystemFont(ofSize: 17)
+        userCreditsLabel.textColor = .black
+        userCreditsLabel.textAlignment = .center
     }
-
+    
     /// Метод настройки таблицы купленных предметов
-    func setupBoughtItemsTableView() {
-        itemsTableView.dataSource = self
-        itemsTableView.delegate = self
-        itemsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-
-        itemsTableView.backgroundColor = .clear
-        itemsTableView.separatorStyle = .none
-        itemsTableView.layer.cornerRadius = 8
+    func setupUserItemsTableView() {
+        userItemsTableView.dataSource = self
+        userItemsTableView.delegate = self
+        userItemsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        userItemsTableView.backgroundColor = .clear
+        userItemsTableView.separatorStyle = .none
+        userItemsTableView.layer.cornerRadius = 8
     }
-
+    
     /// Метод добавления элементов интерфейса на главный экран и отключения масок AutoLayout
     func addSubviews() {
         view.addSubviews(
             userImageView,
-            walletLabel,
-            itemsTableView
+            userCreditsLabel,
+            userItemsTableView
         )
-
+        
         view.disableAutoresizingMask(
             userImageView,
-            walletLabel,
-            itemsTableView
+            userCreditsLabel,
+            userItemsTableView
         )
     }
 }
@@ -142,7 +183,7 @@ private extension InfoCharacterViewController {
 // MARK: - Constraints
 
 private extension InfoCharacterViewController {
-
+    
     /// Метод установки констреинтов элементов интерфейса
     func setConstraints() {
         NSLayoutConstraint.activate([
@@ -150,16 +191,16 @@ private extension InfoCharacterViewController {
             userImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             userImageView.heightAnchor.constraint(equalToConstant: 150),
             userImageView.widthAnchor.constraint(equalToConstant: 150),
-
-            walletLabel.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 32),
-            walletLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            walletLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            walletLabel.heightAnchor.constraint(equalToConstant: 22),
-
-            itemsTableView.topAnchor.constraint(equalTo: view.centerYAnchor),
-            itemsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            itemsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            itemsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            
+            userCreditsLabel.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 32),
+            userCreditsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userCreditsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            userCreditsLabel.heightAnchor.constraint(equalToConstant: 22),
+            
+            userItemsTableView.topAnchor.constraint(equalTo: view.centerYAnchor),
+            userItemsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userItemsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            userItemsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
     }
 }
@@ -167,36 +208,36 @@ private extension InfoCharacterViewController {
 // MARK: - TableView DataSource
 
 extension InfoCharacterViewController: UITableViewDataSource {
-
+    
     /// Метод определения количества секций таблицы
     func numberOfSections(in tableView: UITableView) -> Int {
         user.items.count
     }
-
+    
     /// Метод присвоения названий секциям таблицы
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         user.items[section].title
     }
-
+    
     /// Метод определения количества ячеек внутри одной секции таблицы
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
     }
-
+    
     /// Метод настройки ячейки таблицы
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = itemsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
+        let cell = userItemsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
         var content = cell.defaultContentConfiguration()
         content.text = user.items[indexPath.section].description
         content.secondaryText = "Sell: \(user.items[indexPath.section].price)"
-
+        
         cell.backgroundColor = .white
         cell.layer.cornerRadius = 8
         cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-
+        
         cell.contentConfiguration = content
-
+        
         return cell
     }
 }
@@ -204,41 +245,41 @@ extension InfoCharacterViewController: UITableViewDataSource {
 // MARK: - TableView Delegate
 
 extension InfoCharacterViewController: UITableViewDelegate {
-
+    
     /// Метод настройки отображения хедера секции
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let itemNameLabel = UILabel(frame: CGRect(x: 17, y: 3, width: tableView.frame.width, height: 20))
         itemNameLabel.text = "\(user.items[section].title)"
         itemNameLabel.font = UIFont.boldSystemFont(ofSize: 17)
         itemNameLabel.textColor = .gray
-
+        
         let contentView = UIView()
         contentView.layer.cornerRadius = 8
         contentView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         contentView.addSubview(itemNameLabel)
-
+        
         return contentView
     }
-
+    
     /// Метод настройки фона хедера секции
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         view.backgroundColor = .tertiarySystemGroupedBackground
     }
-
+    
     /// Метод настройки поведения приложения при нажатии на ячейку таблицы
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.selectedBackgroundView?.layer.cornerRadius = 8
             cell.selectedBackgroundView?.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         }
-
+        
         showAlert(withTitle: "Sell this item", andMessage: "Do you really want to sell it?") { [weak self] action in
             switch action {
             case .confirm:
                 guard let itemPrice = self?.user.items[indexPath.section].price else { return }
                 self?.user.wallet += itemPrice
                 self?.user.items.remove(at: indexPath.section)
-                self?.updateUserPic()
+                self?.updateUserImage()
                 self?.updateCreditsValue()
             case .refuse:
                 tableView.deselectRow(at: indexPath, animated: true)
@@ -250,28 +291,49 @@ extension InfoCharacterViewController: UITableViewDelegate {
 // MARK: - Alert Controller
 
 private extension InfoCharacterViewController {
-
+    
     /// Действия алерт-контроллера
     enum AlertAction {
         case confirm
         case refuse
     }
-
+    
     /// Метод настройки алерт-контроллера
     func showAlert(withTitle title: String, andMessage message: String, _ handler: @escaping (AlertAction) -> Void) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-
+        
         let confirmAction = UIAlertAction(title: "Confirm", style: .default) { _ in
             handler(.confirm)
         }
         let refuseAction = UIAlertAction(title: "Refuse", style: .destructive) { _ in
             handler(.refuse)
         }
-
+        
         alert.addAction(confirmAction)
         alert.addAction(refuseAction)
-
+        
         present(alert, animated: true)
+    }
+}
+
+// MARK: - Constants
+
+private extension InfoCharacterViewController {
+
+    /// Имена изображений
+    enum StageImages {
+        static let images = [
+            "stage01",
+            "stage02",
+            "stage03",
+            "stage04"
+        ]
+    }
+
+    /// Имена звуков
+    enum Sounds {
+        static let levelUp = "levelUp"
+        static let levelDown = "levelDown"
     }
 }
 
