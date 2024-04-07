81769acbf4003ad826c0d134fb3466a40ca7a4df
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2024-04-07 18:41:53 +0500

    Изменения: - исправил константу tableViewSecondaryText

--- UiLuxury_TeamProject/Scenes/MainScene/UserScene/UserViewController.swift
+++ UiLuxury_TeamProject/Scenes/MainScene/UserScene/UserViewController.swift
@@ -103,7 +103,7 @@ extension UserViewController: UITableViewDataSource {
         
         var content = cell.defaultContentConfiguration()
         content.text = viewModel.getText(indexPath: indexPath)
-        content.secondaryText = "\(Text.tableViewSecondaryText): $\(viewModel.getSecondaryText(indexPath: indexPath))"
+        content.secondaryText = "\(Text.tableViewSecondaryText)\(viewModel.getSecondaryText(indexPath: indexPath))"
         content.secondaryTextProperties.font = .boldSystemFont(ofSize: 17)
         
         setupUserItemsTableViewCell(cell)
@@ -328,8 +328,8 @@ private extension UserViewController {
     enum Text {
         static let cellIdentifier = "cell"
         static let currentStage = "СТАДИЯ: "
-        static let tableViewSecondaryText = "Продать:"
-        
+        static let tableViewSecondaryText = "Продать: $"
+
         static let alertTitle = "Продать?"
         static let alertMessage = ""
         static let alertConfirmTitle = "Да"
