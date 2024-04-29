6c0acd9ed9e632a8bd572d0251c35f157b1ac980
Iurii Kurinnoi <yur4kur@icloud.com>
2024-04-29 11:08:47 +0400

    Добавлен базовый протокол вью-моделей.

---
+++ UiLuxury_TeamProject/Helpers/Protocols/ViewModelType.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModelType.swift
+//  UiLuxury_TeamProject
+//
+//  Created by Юрий Куринной on 29.04.2024.
+//
+
+/// Базовый протокол вью-моделей
+protocol ViewModelType {
+    
+    /// Поток данных, приходящих из вью-контроллера
+    associatedtype Input
+    
+    /// Поток данных, возвращаемых вью-моделью
+    associatedtype Output
+    
+    /// Коллбэк для биндинга свойств
+    var modelDidChange: (AnyObject) -> Void { get set }
+    
+    /// Запрос на обработку данных вью-моделью
+    func transform(input: Input) -> Output
+}
